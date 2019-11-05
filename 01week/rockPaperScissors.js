86c23568d67920fdfff098690a42d2a9a1c75f7c
Oscar Garcia <oscar.gch@gmail.com>
2019-11-05 17:43:36 -0600

    updated the test

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -10,12 +10,18 @@ const rl = readline.createInterface({
 
 function rockPaperScissors(hand1, hand2) {
  
-var hand1 = hand1.toLowerCase();
-var hand2 = hand2.toLowerCase();
+  var hand1 = hand1.toLowerCase();
+  var hand2 = hand2.toLowerCase();
+  
+  var hand1 = hand1.trim();
+  var hand2 = hand2.trim();
 
-var hand1 = hand1.trim();
-var hand2 = hand2.trim();
+let validArguments = ["rock", "paper", "scissors"];
 
+if (!validArguments.includes(hand1) || !validArguments.includes(hand2)) {
+  return "Not a valid entry.";
+}
+ 
   if (hand1 === hand2) {
     return "It's a tie!";
   } else if (hand1 === "rock" && hand2 === "scissors") {
@@ -62,6 +68,19 @@ if (typeof describe === 'function') {
       assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
       assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
       assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
+    });
+    it('should detect all possible scenarios in which "Hand one wins!"', () => {
+      assert.equal(rockPaperScissors('rock', ' scissors '), "Hand one wins!");
+      assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
+      assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
+    });
+    it('should detect all possible scenarios in which "Hand two wins!"', () => {
+      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+      assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
+    });
+    it('Test to make sure user must input a valid entry (e.g. "rock", "paper", or "scissors")', () => {
+      assert.equal(rockPaperScissors('dog', 'cat'), "Not a valid entry.");
     });
   });
 } else {
